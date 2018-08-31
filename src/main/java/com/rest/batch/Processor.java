9b3dafb93104e8251353d74d7e2329fdd5782372
Kanav Mehra <kanavmehra957@gmail.com>
2018-08-31 13:05:01 +0530

    diff mails to M and F executed

--- src/main/java/com/rest/batch/Processor.java
+++ src/main/java/com/rest/batch/Processor.java
@@ -1,13 +1,11 @@
 package com.rest.batch;
 
 
-import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -24,23 +22,30 @@ public class Processor implements ItemProcessor<User, User>{
 	
 	@Override
 	public User process(User user) throws Exception {
-		MimeMessage message = sender.createMimeMessage();
-		try {
+		    MimeMessage message = sender.createMimeMessage();
+		
 			MimeMessageHelper helper = new MimeMessageHelper(message, true);
-			helper.setTo(user.getEmailId());
-			helper.setText(
-					"<html><body>Hi <b>"+user.getName()+"</b> ,</br>Welcome to Caltex</br></br> Thanks for using our services </br>See you around</br>Team Caltex<img src='cid:id101'/><body></html>",
-					true);
-			helper.setSubject("Welcome to Caltex");
-			ClassPathResource file1 = new ClassPathResource("Caltexlogo.png");
-			helper.addInline("id101", file1);
+			if(user.getGender()=="M") {
+				helper.setTo(user.getEmailId());
+				helper.setText(
+						"<html><body>Hi <b>"+user.getName()+"</b> ,</br>Welcome to Caltex</br></br> Thanks for using our services </br>See you around</br>Team Caltex<img src='cid:id101'/><body></html>",
+						true);
+				helper.setSubject("Welcome to Caltex");
+				ClassPathResource file1 = new ClassPathResource("Caltexlogo.png");
+				helper.addInline("id101", file1);
+			}
+			else {
+				helper.setTo(user.getEmailId());
+				helper.setText(
+						"<html><body>Hi <b>"+user.getName()+"</b> ,</br>Welcome to amazon</br></br> Thanks for using our services </br>See you around</br>Team amazon<img src='cid:id101'/><body></html>",
+						true);
+				helper.setSubject("Welcome to amazon");
+				ClassPathResource file1 = new ClassPathResource("amazonlogo.jpg");
+				helper.addInline("id101", file1);
+			}
 			
 			sender.send(message);
-		} catch (MailException e) {
-			e.printStackTrace();
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
+		
 		return user;
 	}
 
--- src/main/java/com/rest/config/SpringBatchConfig.java
+++ src/main/java/com/rest/config/SpringBatchConfig.java
@@ -27,32 +27,30 @@ public class SpringBatchConfig {
 			ItemReader<User> itemReader,ItemWriter<User> itemWriter,ItemProcessor<User, User> itemProcessor) {
 		
 		Step step1 = stepBuilderFactory.get("ETL-Database-Load-male")
-				.<User,User> chunk(1)
+				.<User,User> chunk(50)
 				.reader(itemReader)
 				.processor(itemProcessor)
 				.writer(itemWriter)
 				.build();
 		
-		Step step2 = stepBuilderFactory.get("ETL-Database-Load-female")
+		/*Step step2 = stepBuilderFactory.get("ETL-Database-Load-female")
 				.<User,User> chunk(1)
 				.reader(itemReader)
 				.processor(itemProcessor)
 				.writer(itemWriter)
-				.build();
+				.build();*/
 				
 				
 		return jobBuilderFactory.get("ETL-Load")
 				.incrementer(new RunIdIncrementer())
-				.flow(step1)
-				.next(step2)
-				.end()
+				.start(step1)
 				.build();
 		
 	}
 	
-	private static final String QUERY_FIND_MALE = "SELECT * from user where gender = 'M'";
+	private static final String QUERY_FIND_MALE = "SELECT * from user";
 	
-	private static final String QUERY_FIND_FEMALE = "SELECT * from user where gender = 'F'";
+	//private static final String QUERY_FIND_FEMALE = "SELECT * from user where gender = 'F'";
 	
 	@Bean
 	ItemReader<User> databaseItemReader (DataSource datasource){
@@ -64,13 +62,5 @@ public class SpringBatchConfig {
 		return databaseReader;
 	}
 	
-	@Bean
-	ItemReader<User> databaseItemReaderFemale (DataSource datasource){
-		JdbcCursorItemReader<User> databaseReaderFemale = new JdbcCursorItemReader<>();
-		databaseReaderFemale.setDataSource(datasource);
-		databaseReaderFemale.setSql(QUERY_FIND_FEMALE);
-		databaseReaderFemale.setRowMapper(new BeanPropertyRowMapper<>(User.class));
-		
-		return databaseReaderFemale;
-	}
+	
 }
